156223f6bd605a825eb65efb38762b905f9054f7
JonathanHaws <jonathanmhaws1@gmail.com>
2023-04-01 05:27:58 -0600

    opengl and glfw working after finally figuring out how to add all the boring windows dependecies to the compile batch

--- compile.bat
+++ compile.bat
@@ -1,9 +1,8 @@
-
 set compiler_path=clang++
 set input_file=main.cpp
 set output_file=main.exe
 
 taskkill /F /IM %output_file% /T 
 if exist %output_file% ( del /F %output_file%)
-%compiler_path% %input_file% -o %output_file% -mwindows -luser32 -lgdi32
-start %output_file%
+%compiler_path% %input_file% -o %output_file% -Iinclude -Llib -lglfw3 -lopengl32 -lgdi32 -luser32 -lmsvcrt -lshell32
+start /B %output_file%
--- main.cpp
+++ main.cpp
@@ -1,53 +1,19 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-#include <windows.h>
+int main() {
+    if (!glfwInit()) { std::cout << "Failed to initialize GLFW" << std::endl; return -1;}
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL GRAPHICS!", NULL, NULL);
+    if (!window) { std::cout << "Failed to create window" << std::endl; glfwTerminate(); return -1;}
+    glfwMakeContextCurrent(window);
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Define the window class
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = "MyWindowClass";
-
-    // Register the window class
-    if (!RegisterClass(&wc)) {
-        MessageBox(NULL, "Failed to register window class", "Error", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    // Create the window
-    HWND hwnd = CreateWindowEx(
-        0,
-        "MyWindowClass",
-        "My Window",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
-        NULL, NULL, hInstance, NULL
-    );
-
-    if (!hwnd) {
-        MessageBox(NULL, "Failed to create window", "Error", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    // Show the window
-    ShowWindow(hwnd, nCmdShow);
-
-    // Message loop
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return msg.wParam;
+    glfwTerminate();
+    return 0;
 }
