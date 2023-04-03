d3476a8b05fa2f82767198fc206bed2222d41e33
JonathanHaws <jonathanmhaws1@gmail.com>
2023-04-03 04:31:37 -0600

    undo system

--- main.cpp
+++ main.cpp
@@ -18,8 +18,13 @@ int main() {
     #pragma region Setup Variables
         glfwSetScrollCallback(window, scroll_callback);
         double mouseX = 0.0, mouseY = 0.0;
+        double lastMouseX = 0.0, lastMouseY = 0.0;
+        bool mouseLeft = false, mouseMiddle = false, mouseRight = false;
+        bool lastMouseLeft = mouseLeft;
         float zoom = 1;
         double scrollYOffset = 0.0;
+        bool z = false; 
+        bool lastZ = false;
 
         struct Color { float r, g, b; };
         Color brush = {0.1f, 0.1f, 0.1f};
@@ -30,20 +35,44 @@ int main() {
         int windowWidth, windowHeight;
         glfwGetWindowSize(window, &windowWidth, &windowHeight);
         float canvasX = windowWidth / 2 - canvasWidth / 2, canvasY = windowHeight / 2 - canvasHeight / 2;
+
+        struct PixelChange { int x, y; Color color; };
+        struct Stroke { std::vector<PixelChange> changes; };
+        std::vector<Stroke> history;
+
         #pragma endregion
 
     while (!glfwWindowShouldClose(window)) {
     
         #pragma region Input
             glfwPollEvents();
-            int lastMouseX = mouseX, lastMouseY = mouseY;
+            lastMouseX = mouseX, lastMouseY = mouseY;
             glfwGetCursorPos(window, &mouseX, &mouseY);
-            bool mouseDown = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
-            bool mouseLeft = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT); 
-            bool mouseMiddle = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS;
-            bool mouseRight = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
+            lastMouseLeft = mouseLeft;
+            mouseLeft = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT); 
+            mouseMiddle = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS;
+            mouseRight = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
+            lastZ = z;
+            z = glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS;
+            bool ctrl = glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS;
+
             #pragma endregion
-           
+
+        #pragma region Undo
+            if (z && ctrl && !lastZ) {
+                if (!history.empty()) {
+                    Stroke& lastStroke = history.back();
+                    for (auto changeIt = lastStroke.changes.rbegin(); changeIt != lastStroke.changes.rend(); ++changeIt) {
+                        const PixelChange& change = *changeIt;
+                        canvas[change.x][change.y] = change.color;
+                    }
+                    history.pop_back();
+                }
+            }
+
+            if (history.size() > 100) { history.erase(history.begin()); } 
+            #pragma endregion
+
         #pragma region Drawing Canvas
 
             #pragma region Setup Context
@@ -72,11 +101,15 @@ int main() {
             #pragma endregion
 
         #pragma region Painting
-            if (mouseDown) {
+            if (mouseLeft) {
                 int startX = (int)((lastMouseX / zoom) - canvasX), startY = (int)((lastMouseY / zoom) - canvasY);
                 int endX = (int)((mouseX / zoom) - canvasX), endY = (int)((mouseY / zoom) - canvasY);
                 float distance = std::sqrt(std::pow(endX - startX, 2) + std::pow(endY - startY, 2));
                 int num_steps = std::ceil(distance);
+
+                if(!lastMouseLeft) { history.push_back(Stroke());} // New stroke
+                Stroke& currentStroke = history.back();
+
                 for (int step = 0; step <= num_steps; ++step) {
                     float t = static_cast<float>(step) / num_steps;
                     int x = static_cast<int>(startX + t * (endX - startX));
@@ -86,7 +119,9 @@ int main() {
                             int xWithOffset = x + offsetX;
                             int yWithOffset = y + offsetY;
                             if (xWithOffset >= 0 && xWithOffset < canvas.size() && yWithOffset >= 0 && yWithOffset < canvas[xWithOffset].size()) {
+                                currentStroke.changes.push_back({xWithOffset, yWithOffset, canvas[xWithOffset][yWithOffset]});
                                 canvas[xWithOffset][yWithOffset] = brush;
+                                
                             }
                         }
                     }
@@ -104,7 +139,7 @@ int main() {
         #pragma region Zoom with Scroll Wheel
             if (mouseScroll != 0) {
                 float oldZoom = zoom;
-                zoom += mouseScroll * 0.1f;
+                zoom += mouseScroll * 0.2f * zoom;
                 zoom = std::max(0.1f, zoom); // Prevent zoom from going negative
                 mouseScroll = 0;
                 canvasX -= (mouseX / oldZoom) - (mouseX / zoom);
